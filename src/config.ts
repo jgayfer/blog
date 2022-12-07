1984f93c1820a5db2c4837515e81b2714b4a7547
James Gayfer <gayfer.james@gmail.com>
2022-12-07 15:26:55 -0800

    Set site title + description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'James Gayfer';
+export const SITE_DESCRIPTION = "I'm a software developer that loves to learn and create. I've always got something on the go!";
